31755ad7eea9898cf274ac4b133c415af5df1041
Faizan Ahmad Khan <ahmadfaizan1604@gmail.com>
2021-09-21 21:16:10 +0530

    Made Cart, Login & Register Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import Home from "./pages/Home";
-
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      {/* <Login /> */}
+      {/* <Cart /> */}
+      {/* <ProductList /> */}
+      {/* <Product /> */}
+      {/* <Register /> */}
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { Badge } from "@material-ui/core";
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
 
 const Container = styled.div`
@@ -13,6 +14,7 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  ${mobile({ padding: "10px 0px" })}
 `;
 
 const Left = styled.div`
